a5d1fda3da36ac40ab1326006d6df42fbaca52dc
Nishan <Nishanshrestha1456@gmail.com>
2024-09-13 18:11:06 +0545

    Loading bar added

--- src/App.js
+++ src/App.js
@@ -10,25 +10,39 @@ import {
   Route
   
 } from "react-router-dom";
-
+import LoadingBar from 'react-top-loading-bar'
 
 
 export default class App extends Component {
   pageSize=5;
+
+  state={
+    progress:0
+  }
+  setProgress=(progress)=>
+  {
+    this.setState({progress:progress})
+  }
   render() {
     return (
       <div>
         <Router>
         <NavBar/>
+        <LoadingBar
+        height={3}
+        color='#f11946'
+        progress={this.state.progress}
+       
+      />
         <Routes>
-        <Route exact path="/" element={ <News key="general" pageSize={this.pageSize} country="us" category="general"/>}></Route>
-          <Route exact path="/General" element={ <News key="general" pageSize={this.pageSize} country="us" category="general"/>}></Route>
-          <Route exact path="/Business" element={ <News key="business" pageSize={this.pageSize} country="us" category="business"/>}></Route>
-          <Route exact path="/Entertainment" element={<News key="entertainment" pageSize={this.pageSize} country="us" category="entertainment"/>}> </Route>
-          <Route exact path="/Health" element={<News key="health" pageSize={this.pageSize} country="us" category="health"/>}></Route>
-          <Route exact path="/Science" element={ <News key="science" pageSize={this.pageSize} country="us" category="science"/>}></Route>
-          <Route exact path="/Sports" element={ <News key="sports" pageSize={this.pageSize} country="us" category="sports"/>}></Route>
-          <Route exact path="/Technology" element={<News key="technology" pageSize={this.pageSize} country="us" category="technology"/>}></Route>
+        <Route exact path="/" element={ <News setProgress={this.setProgress} key="general" pageSize={this.pageSize} country="us" category="general"/>}></Route>
+          <Route exact path="/General" element={ <News setProgress={this.setProgress} key="general" pageSize={this.pageSize} country="us" category="general"/>}></Route>
+          <Route exact path="/Business" element={ <News setProgress={this.setProgress}  key="business" pageSize={this.pageSize} country="us" category="business"/>}></Route>
+          <Route exact path="/Entertainment" element={<News setProgress={this.setProgress} key="entertainment" pageSize={this.pageSize} country="us" category="entertainment"/>}> </Route>
+          <Route exact path="/Health" element={<News setProgress={this.setProgress} key="health" pageSize={this.pageSize} country="us" category="health"/>}></Route>
+          <Route exact path="/Science" element={ <News setProgress={this.setProgress} key="science" pageSize={this.pageSize} country="us" category="science"/>}></Route>
+          <Route exact path="/Sports" element={ <News setProgress={this.setProgress} key="sports" pageSize={this.pageSize} country="us" category="sports"/>}></Route>
+          <Route exact path="/Technology" element={<News setProgress={this.setProgress} key="technology" pageSize={this.pageSize} country="us" category="technology"/>}></Route>
          
         </Routes>
         
--- src/components/News.js
+++ src/components/News.js
@@ -38,16 +38,20 @@ capitalizeFirst=(string)=>
   
     async updateNews()
     {
-      
+      this.props.setProgress(10);
       let url =`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=5d01b62106c444ab80e5044044abaeb0&page=${this.state.page}&pageSize=${this.props.pageSize}`;
       this.setState({loading:true});
       let data= await fetch(url);
+      this.props.setProgress(30);
       let parsedData=await data.json();
+      this.props.setProgress(70);
       this.setState({
           articles:parsedData.articles,
           totalResults:parsedData.totalResults,
           loading:false
       })
+      this.props.setProgress(100);
+
     }
    async componentDidMount()
     {
